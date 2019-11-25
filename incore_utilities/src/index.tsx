42c26f9a4186fb47b944d257b0989b40fd77f6c9
Chen Wang <cwang138@illinois.edu>
2019-11-25 16:27:52 -0600

    remove the login widet

--- incore_utilities/src/index.tsx
+++ incore_utilities/src/index.tsx
@@ -2,19 +2,11 @@ import {ILayoutRestorer, JupyterFrontEnd, JupyterFrontEndPlugin} from '@jupyterl
 
 import { IMainMenu } from '@jupyterlab/mainmenu';
 
-import {IFrame, ICommandPalette, WidgetTracker} from '@jupyterlab/apputils';
+import {IFrame, ICommandPalette} from '@jupyterlab/apputils';
 
 import {Menu} from '@phosphor/widgets';
 
-import {appendCredentials} from './action';
-
 import { ILauncher } from '@jupyterlab/launcher';
-
-import {JSONExt} from '@phosphor/coreutils';
-
-import {Widget} from '@phosphor/widgets';
-
-import {IncoreLoginWidget} from "./IncoreLoginWidget";
 
 import '../style/index.css';
 
@@ -30,13 +22,13 @@ async function activate(app: JupyterFrontEnd, mainMenu: IMainMenu,
             label: item.name,
             execute: () =>{
                 if (item.target == '_blank'){
-                    let win = window.open(item.url + "?" + queryString, '_blank');
+                    let win = window.open(item.url, '_blank');
                     win.focus();
                 }
                 else if (item.target == 'widget'){
                     if (!iframe){
                         iframe = new IFrame();
-                        iframe.url = item.url + "?" + queryString;
+                        iframe.url = item.url;
                         iframe.id = item.name;
                         iframe.title.label = item.name;
                         iframe.title.closable = true;
@@ -61,62 +53,14 @@ async function activate(app: JupyterFrontEnd, mainMenu: IMainMenu,
 
     }
 
-    // if user.json exists, queryString will be ?&user=xxx&auth-token=xxxx; else queryString is empty
-    let queryString = await appendCredentials();
-    if (queryString !== ""){
-        menuItem.forEach(item => appendNewCommand(item));
-    }
-
+    menuItem.forEach(item => appendNewCommand(item));
     docItem.forEach(item => appendNewCommand(item));
 
     let menu = Private.createMenu(app);
     let doc = Private.createDoc(app);
 
-    /* add login widget */
-    let LoginWidget: IncoreLoginWidget;
-    const commandLogin: string="incore-login:open";
-
-    app.commands.addCommand(commandLogin, {
-        label: 'INCORE login',
-        caption:'INCORE login',
-        iconClass: 'jp-IncoreIcon',
-        execute: () => {
-            if (!LoginWidget) {
-                LoginWidget = new IncoreLoginWidget();
-                LoginWidget.update();
-            }
-            if (!trackerLogin.has(LoginWidget)) {
-                trackerLogin.add(LoginWidget);
-            }
-            if (!LoginWidget.isAttached) {
-                LoginWidget = new IncoreLoginWidget();
-                LoginWidget.update();
-                app.shell.add(LoginWidget, 'main');
-            } else {
-                LoginWidget.update();
-            }
-
-            app.shell.activateById(LoginWidget.id);
-        }
-    });
-
-
-    /* add menus and login to jupyterlab element: mainMenu, palette, and launcher */
-    palette.addItem({ command: commandLogin, category: 'Other' });
-    launcher.add({ command: commandLogin,  category: 'Other' });
-
-    // Add the login to the menu and then add menu to mainMenu
-    menu.addItem({command: commandLogin});
     mainMenu.addMenu(menu, {rank:0});
     mainMenu.addMenu(doc, {rank:0});
-
-    /* Track and restore the widget state */
-    let trackerLogin = new WidgetTracker<Widget>({ namespace: 'incore_login' });
-    restorer.restore(trackerLogin, {
-        command: commandLogin,
-        args: () => JSONExt.emptyObject,
-        name: () => 'incore_login'
-    });
 
     return Promise.resolve(void 0);
 }
@@ -156,47 +100,47 @@ const extension: JupyterFrontEndPlugin<void> = {
     activate: activate
 };
 
+let hostDomainName = window.location.origin;
 /**
  * Initialize the menu data part
  */
 export const menuItem = [
     {
         name: 'Fragility Explorer',
-        url: 'https://incore2.ncsa.illinois.edu/FragilityViewer',
+        url: `${hostDomainName}/FragilityViewer`,
         description:'fragility explorer standalone app',
         target: 'widget'
     },
     {
         name: 'Data Explorer',
-        url: 'https://incore2.ncsa.illinois.edu/DataViewer',
+        url: `${hostDomainName}/DataViewer`,
         description:'data explorer standalone app',
         target: 'widget'
     },
     {
         name: 'Hazard Explorer',
-        url: 'https://incore2.ncsa.illinois.edu/HazardViewer',
+        url: `${hostDomainName}/HazardViewer`,
         description:'hazard explorer standalone app',
         target: 'widget'
     }
 ];
 
-// TODO: documentation url might change
 export const docItem = [
     {
         name:'IN-CORE Manual',
-        url:'https://incore2.ncsa.illinois.edu/doc/incore/index.html',
+        url:`${hostDomainName}/doc/incore/index.html`,
         description:'About IN-CORE project',
         target:'widget'
     },
     {
         name:'pyIncore Reference',
-        url:'https://incore2.ncsa.illinois.edu/doc/pyincore',
+        url:`${hostDomainName}/doc/pyincore`,
         description:'Sphinx documentation for pyincore library',
         target:'widget'
     },
     {
         name:'IN-CORE Web Service API',
-        url:'https://incore2.ncsa.illinois.edu/doc/api/',
+        url:`${hostDomainName}/doc/api/`,
         description:'Swagger documentation for incore web services',
         target:'widget'
     }
